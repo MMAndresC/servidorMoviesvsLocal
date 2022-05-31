73ede9c099c1aec1a16253be441e0b36f9fbfa6d
MMAndresC <mandrescasabon@hotmail.com>
2022-05-31 22:34:36 +0200

    REFACTOR function search by

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -16,32 +16,28 @@ router.get('/movies',(req,res) =>{ //4 Crear un endpoint get que devuelva todas 
     return res.send(movies[movieId]);
 })
 
-const searchBy = (criteria,data) =>{ 
+const searchBy = (criteria,data,res) =>{ 
     const result = [];
     for(const movie of movies){
         if(movie[criteria] == data){
             result.push(movie);
         }
     }    
-    return result;
+    if (!!result.length){
+        return res.send(result);
+    }else{
+        return res.send('No existe esa pelicula en la db');
+    }
 }
 
 router.get('/movies/title/:title',(req,res) =>{ //6 Crear un endpoint get que devuelva un valor por su titulo.
     const {title} = req.params;
-    const find = searchBy('title',title);
-    if (find){
-        return res.send(find);
-    }
-    return res.send('No existe esa pelicula en la db');
+    searchBy('title',title,res);
 })
 
 router.get('/movies/genre/:genre',(req,res) =>{  //7 Crear un endpoint get que devuelva los documentos según su género.
     const {genre} = req.params;
-    const find = searchBy('genre',genre);
-    if (find){
-        return res.send(find);
-    }
-    return res.send('No existe esa pelicula en la db');     
+    searchBy('genre',genre,res);    
 })
 
 router.get('/movies/year',(req,res) => {  //8 Crear un endpoint get que devuelva las películas que se han estrenado a partir de 2010.
@@ -57,7 +53,6 @@ router.get('/movies/year',(req,res) => {  //8 Crear un endpoint get que devuelva
         return res.send('No existe esa pelicula en la db');     
     } 
 })
-
 
 
 router.get('/',(req,res) =>{
