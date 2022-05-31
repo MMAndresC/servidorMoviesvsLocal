78c801fe1496edfa05ec5da238877ab8962acd08
MMAndresC <mandrescasabon@hotmail.com>
2022-05-31 23:49:27 +0200

    FIXED mensaje de error en year y cambiados for por filter

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -17,12 +17,7 @@ router.get('/movies',(req,res) =>{ //4 Crear un endpoint get que devuelva todas 
 })
 
 const searchBy = (criteria,data,res) =>{ 
-    const result = [];
-    for(const movie of movies){
-        if(movie[criteria] == data){
-            result.push(movie);
-        }
-    }    
+    const result = movies.filter(movie => movie[criteria] == data);
     if (!!result.length){
         return res.send(result);
     }else{
@@ -41,16 +36,11 @@ router.get('/movies/genre/:genre',(req,res) =>{  //7 Crear un endpoint get que d
 })
 
 router.get('/movies/year',(req,res) => {  //8 Crear un endpoint get que devuelva las películas que se han estrenado a partir de 2010.
-    const resultYear = [];
-    for(const movie of movies){
-        if(movie.year > 2010){
-            resultYear.push(movie);
-        }
-    }
-    if(resultYear){
-        return res.send(resultYear);
+    const result = movies.filter(movie => movie.year > 2010);
+    if(result.length >0){
+        return res.send(result);
     }else{
-        return res.send('No existe esa pelicula en la db');     
+        return res.send('No hay en catalogo peliculas a partir de 2010');            
     } 
 })
 
