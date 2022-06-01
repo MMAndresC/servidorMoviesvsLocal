0694ad63831446e75a9e94a3dddbf8a8fc959191
MMAndresC <mandrescasabon@hotmail.com>
2022-06-01 10:23:19 +0200

    added another routes file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const PORT = 4500;
-const indexRouter = require('./routes/indexRouter'); 
+const movieRouter = require('./routes/moviesRoutes'); 
+const indexRouter = require('./routes/indexRoutes');
 
 
 const app = express();
 
-app.use('/',indexRouter);
+app.use('/movies',movieRouter);
+app.use('/', indexRouter);
 
 app.listen(PORT, () =>{
     console.log('Working');
--- routes/moviesRoutes.js
+++ routes/moviesRoutes.js
@@ -4,11 +4,12 @@ const router = express.Router();
 
 const movies = require('../movies');
 
-router.get('/movies',(req,res) =>{ //4 Crear un endpoint get que devuelva todas las películas.
+
+router.get('/',(req,res) =>{ //4 Crear un endpoint get que devuelva todas las películas.
     return res.send(movies);
 })
 
- router.get('/movies/id/:id',(req,res)=>{ //5 rear un endpoint get que devuelva una película según su _id
+ router.get('/id/:id',(req,res)=>{ //5 rear un endpoint get que devuelva una película según su _id
     const movieId = req.params.id;
     if(!movies[movieId]){
         return res.send('No existe esa pelicula en la db');
@@ -25,17 +26,17 @@ const searchBy = (criteria,data,res) =>{
     }
 }
 
-router.get('/movies/title/:title',(req,res) =>{ //6 Crear un endpoint get que devuelva un valor por su titulo.
+router.get('/title/:title',(req,res) =>{ //6 Crear un endpoint get que devuelva un valor por su titulo.
     const {title} = req.params;
     searchBy('title',title,res);
 })
 
-router.get('/movies/genre/:genre',(req,res) =>{  //7 Crear un endpoint get que devuelva los documentos según su género.
+router.get('/genre/:genre',(req,res) =>{  //7 Crear un endpoint get que devuelva los documentos según su género.
     const {genre} = req.params;
     searchBy('genre',genre,res);    
 })
 
-router.get('/movies/year',(req,res) => {  //8 Crear un endpoint get que devuelva las películas que se han estrenado a partir de 2010.
+router.get('/year',(req,res) => {  //8 Crear un endpoint get que devuelva las películas que se han estrenado a partir de 2010.
     const result = movies.filter(movie => movie.year > 2010);
     if(result.length >0){
         return res.send(result);
@@ -44,9 +45,5 @@ router.get('/movies/year',(req,res) => {  //8 Crear un endpoint get que devuelva
     } 
 })
 
-
-router.get('/',(req,res) =>{
-    return res.send('Raiz');
-})
 
 module.exports = router;
